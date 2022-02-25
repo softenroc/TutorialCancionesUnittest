358be3a44da99306f180e1e27ac3eeb3ca4f4a30
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-02-24 23:35:36 -0500

    TDD tutorial pruebas unitarias en python

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -6,32 +6,43 @@ from src.modelo.declarative_base import engine, Base, session
 
 class Coleccion():
 
-    def __init__(self):
-        Base.metadata.create_all(engine)
+   def __init__(self):
+       Base.metadata.create_all(engine)
 
-    def agregar_album(self, titulo, anio, descripcion, medio):
-        busqueda = session.query(Album).filter(Album.titulo == titulo).all()
-        if len(busqueda) == 0:
-            album = Album(titulo=titulo, ano=anio, descripcion=descripcion, medio=medio)
-            session.add(album)
-            session.commit()
-            return True
-        else:
-            return False
+   def agregar_album(self, titulo, anio, descripcion, medio):
+       busqueda = session.query(Album).filter(Album.titulo == titulo).all()
+       if len(busqueda) == 0:
+           album = Album(titulo=titulo, ano=anio, descripcion=descripcion, medio=medio)
+           session.add(album)
+           session.commit()
+           return True
+       else:
+           return False
 
-    def editar_album(self, album_id, titulo, anio, descripcion, medio):
-        busqueda = session.query(Album).filter(Album.titulo == titulo, Album.id != album_id).all()
-        if len(busqueda) == 0:
-            album = session.query(Album).filter(Album.id == album_id).first()
-            album.titulo = titulo
-            album.ano = anio
-            album.descripcion = descripcion
-            album.medio = medio
-            session.commit()
-            return True
-        else:
-            return False
+   def dar_medios(self):
+       return [medio.name for medio in Medio]
 
-    def dar_album_por_id(self, album_id):
-        return session.query(Album).get(album_id).__dict__
+   def editar_album(self, album_id, titulo, anio, descripcion, medio):
+       busqueda = session.query(Album).filter(Album.titulo == titulo, Album.id != album_id).all()
+       if len(busqueda) == 0:
+           album = session.query(Album).filter(Album.id == album_id).first()
+           album.titulo = titulo
+           album.ano = anio
+           album.descripcion = descripcion
+           album.medio = medio
+           session.commit()
+           return True
+       else:
+           return False
 
+   def eliminar_album(self, album_id):
+       try:
+           album = session.query(Album).filter(Album.id == album_id).first()
+           session.delete(album)
+           session.commit()
+           return True
+       except:
+           return False
+
+   def dar_album_por_id(self, album_id):
+       return session.query(Album).get(album_id).__dict__
